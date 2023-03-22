b672178ddc20b9d69e9b7362dd844831fd0e07aa
David Marqués García <57282198+DavidSiretMarques@users.noreply.github.com>
2023-03-22 16:16:47 +0100

    Delete expresions-matter.py

--- expresions-matter.py
+++
@@ -1,8 +0,0 @@
-"""
-Solution to the CodeWars kata Expressions Matter
-Link:
-https://www.codewars.com/kata/5ae62fcf252e66d44d00008e/python
-"""
-
-def expression_matter(a, b, c):
-    return max((a+b)*c,a+b+c,a*(b+c),a*b*c )
